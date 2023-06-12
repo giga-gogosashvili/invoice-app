6920bd5b97ecf67714f9eda857ccfdb9a4a0a1ea
giga-gogosashvili <gogosashvili@gmail.com>
2023-06-12 23:09:09 +0200

    MUI first elements added

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,7 +2,6 @@ describe("My First Test", () => {
   it("Visit invoice list page", () => {
     cy.visit("http://localhost:3000/");
     cy.contains("h1", "Invoices");
-    cy.contains("p", "are");
-    cy.contains("button", "Read");
+    cy.visit("http://localhost:3000/:id");
   });
 });
--- src/components/Invoices.tsx
+++ src/components/Invoices.tsx
@@ -1,8 +1,34 @@
+import { Box, List, ListItem, ListItemText, ListItemIcon } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FileOpenIcon from "@mui/icons-material/FileOpen";
+import EditIcon from "@mui/icons-material/Edit";
+
 export default function Invoices() {
   return (
-    <div>
+    <Box
+      sx={{
+        // mx: "auto",
+        flexGrow: 1,
+        width: 1200,
+        bgcolor: "#F8F8FB",
+      }}
+    >
       <h1>Invoices</h1>
       <p>There are 7 total invoices</p>
-    </div>
+      <List>
+        <ListItem>
+          <ListItemText primary="List item 1"></ListItemText>
+          <ListItemIcon>
+            <FileOpenIcon />
+          </ListItemIcon>
+          <ListItemIcon>
+            <EditIcon />
+          </ListItemIcon>
+          <ListItemIcon>
+            <DeleteIcon />
+          </ListItemIcon>
+        </ListItem>
+      </List>
+    </Box>
   );
 }
