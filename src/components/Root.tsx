3146139e752903de8c9eaaf8dc258d483648bcd2
giga-gogosashvili <gogosashvili@gmail.com>
2023-06-12 18:05:39 +0200

    fixed route

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Invoices from "./Invoices";
-import { Outlet } from "react-router-dom";
 
 export default function Root() {
-  return <Invoices />;
+  return (
+    <>
+      <Invoices />
+    </>
+  );
 }
