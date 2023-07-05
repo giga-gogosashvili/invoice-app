313d7fd77b77d6790265a2c392c5169451e3d9d0
Pawel <pawel.zwolenski@gmail.com>
2023-07-05 06:55:28 +0200

    try fixing cors

--- backend/main.go
+++ backend/main.go
@@ -9,87 +9,86 @@ import (
 )
 
 type senderAddress struct {
-	Street string `json:"street"`
-	City string `json:"city"`
+	Street   string `json:"street"`
+	City     string `json:"city"`
 	PostCode string `json:"postCode"`
-	Country string `json:"country"`
+	Country  string `json:"country"`
 }
 type clientAddress struct {
-	Street string `json:"street"`
-	City string `json:"city"`
+	Street   string `json:"street"`
+	City     string `json:"city"`
 	PostCode string `json:"postCode"`
-	Country string `json:"country"`
+	Country  string `json:"country"`
 }
 type items struct {
-	Name string `json:"name"`
+	Name     string  `json:"name"`
 	Quantity float64 `json:"quantity"`
-	Price float64 `json:"price"`
-	Total float64 `json:"total"`
+	Price    float64 `json:"price"`
+	Total    float64 `json:"total"`
 }
 type invoice struct {
 	Id            string        `json:"id"`
 	CreatedAt     string        `json:"createdAt"`
-	PaymentDue  string `json:"paymentDue"`
-	Description  string `json:"description"`
-	PaymentTerms  float64 `json:"paymentTerms"`
-	ClientName  string `json:"clientName"`
-	ClientEmail  string `json:"clientEmail"`
-	Status  string `json:"status"`
+	PaymentDue    string        `json:"paymentDue"`
+	Description   string        `json:"description"`
+	PaymentTerms  float64       `json:"paymentTerms"`
+	ClientName    string        `json:"clientName"`
+	ClientEmail   string        `json:"clientEmail"`
+	Status        string        `json:"status"`
 	SenderAddress senderAddress `json:"senderAddress"`
 	ClientAddress clientAddress `json:"clientAddress"`
-	Items []items `json:"items"`
-	Total float64 `json:"total"`
-
+	Items         []items       `json:"items"`
+	Total         float64       `json:"total"`
 }
 
 var invoices = []invoice{
 	invoice{
-		Id: "RT3080", 
-		CreatedAt: "2021-08-18", 
-		PaymentDue: "2021-08-19", 
-		Description: "Re-branding", 
-		PaymentTerms: 1, 
-		ClientName: "Jensen Huang", 
-		ClientEmail: "jensenh@mail.com", 
-		Status: "paid", 
+		Id:           "RT3080",
+		CreatedAt:    "2021-08-18",
+		PaymentDue:   "2021-08-19",
+		Description:  "Re-branding",
+		PaymentTerms: 1,
+		ClientName:   "Jensen Huang",
+		ClientEmail:  "jensenh@mail.com",
+		Status:       "paid",
 		SenderAddress: senderAddress{
-			Street: "19 Union Terrace", 
-			City: "London", 
-			PostCode: "E1 3EZ", 
-			Country: "United Kingdom",
-		}, 
-		ClientAddress: clientAddress{
-			Street: "106 Kendell Street", 
-			City: "Sharrington", 
-			PostCode: "NR24 5WQ", 
-			Country: "United Kingdom",
+			Street:   "19 Union Terrace",
+			City:     "London",
+			PostCode: "E1 3EZ",
+			Country:  "United Kingdom",
 		},
-		Items: []items {
+		ClientAddress: clientAddress{
+			Street:   "106 Kendell Street",
+			City:     "Sharrington",
+			PostCode: "NR24 5WQ",
+			Country:  "United Kingdom",
+		},
+		Items: []items{
 			items{Name: "Brand Guidelines", Quantity: 1, Price: 1800.9, Total: 1800.9},
 		},
 	},
 	invoice{
-		Id: "RT3081", 
-		CreatedAt: "2021-08-18", 
-		PaymentDue: "2021-08-19", 
-		Description: "Re-branding", 
-		PaymentTerms: 1, 
-		ClientName: "Jensen Huang", 
-		ClientEmail: "jensenh@mail.com", 
-		Status: "paid", 
+		Id:           "RT3081",
+		CreatedAt:    "2021-08-18",
+		PaymentDue:   "2021-08-19",
+		Description:  "Re-branding",
+		PaymentTerms: 1,
+		ClientName:   "Jensen Huang",
+		ClientEmail:  "jensenh@mail.com",
+		Status:       "paid",
 		SenderAddress: senderAddress{
-			Street: "19 Union Terrace", 
-			City: "London", 
-			PostCode: "E1 3EZ", 
-			Country: "United Kingdom",
-		}, 
-		ClientAddress: clientAddress{
-			Street: "106 Kendell Street", 
-			City: "Sharrington", 
-			PostCode: "NR24 5WQ", 
-			Country: "United Kingdom",
+			Street:   "19 Union Terrace",
+			City:     "London",
+			PostCode: "E1 3EZ",
+			Country:  "United Kingdom",
 		},
-		Items: []items {
+		ClientAddress: clientAddress{
+			Street:   "106 Kendell Street",
+			City:     "Sharrington",
+			PostCode: "NR24 5WQ",
+			Country:  "United Kingdom",
+		},
+		Items: []items{
 			items{Name: "Brand Guidelines", Quantity: 1, Price: 1800.9, Total: 1800.9},
 		},
 	},
@@ -101,13 +100,27 @@ func main() {
 
 	r.HandleFunc("/invoices", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, YourOwnHeader")
+		}
+		if r.Method == "OPTIONS" {
+			return
+		}
 		json.NewEncoder(w).Encode(invoices)
 	}).Methods("GET")
 
 	r.HandleFunc("/invoices", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, YourOwnHeader")
+		}
+		if r.Method == "OPTIONS" {
+			return
+		}
 		decoder := json.NewDecoder(r.Body)
 		var inv invoice
 		err := decoder.Decode(&inv)
@@ -122,7 +135,14 @@ func main() {
 
 	r.HandleFunc("/invoices/{id}", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, YourOwnHeader")
+		}
+		if r.Method == "OPTIONS" {
+			return
+		}
 		vars := mux.Vars(r)
 		id, ok := vars["id"]
 		if !ok {
@@ -140,7 +160,14 @@ func main() {
 
 	r.HandleFunc("/invoices/{id}", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, YourOwnHeader")
+		}
+		if r.Method == "OPTIONS" {
+			return
+		}
 		vars := mux.Vars(r)
 		id, ok := vars["id"]
 		if !ok {
@@ -159,7 +186,14 @@ func main() {
 
 	r.HandleFunc("/invoices/{id}", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, YourOwnHeader")
+		}
+		if r.Method == "OPTIONS" {
+			return
+		}
 		decoder := json.NewDecoder(r.Body)
 		var newinv invoice
 		err := decoder.Decode(&newinv)
